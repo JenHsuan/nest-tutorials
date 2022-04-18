f3a994329027ed7aa083dd3ca721804c80bf43c4
Sean Hsieh <hsiehs@fortinet.com>
2022-04-18 13:29:51 +0800

    [Auth] add the login API with local guard

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    private readonly users = [
+        Object.assign(new UserDto(), {
+            userId: 1,
+            username: 'username',
+            password: 'password',
+        })
+    ];
+
+    async findOne(username: string): Promise<UserDto | undefined> {
+        return this.users.find(user => user.username === username);
+    }
+}
